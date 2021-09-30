b0185f9f8a450efd4a176757725cb3ad45851ba1
NProlic <nikola.prolic@gmail.com>
2021-10-01 01:42:08 +0200

    New stuff

--- js/main.js
+++ js/main.js
@@ -14,3 +14,35 @@ function closePlayer() {
     document.getElementById("musicPlayer").style.bottom="-160px";
     document.getElementById("downIcon").style.display="none";
 }
+
+/* ------- Artists page ------- */
+
+let _artists = [];
+
+// fetch data from the artists json file
+
+async function fetchData() {
+    const response = await fetch('json/artists.json');
+    const data = await response.json();
+    _artists = data;
+    console.log(_artists);
+    appendArtists(_artists);
+    showLoader(false);
+}
+
+fetchData();
+
+function appendArtists(artists) {
+    let htmlTemplate = "";
+    for (let artist of artists) {
+        htmlTemplate += /*html*/`
+        <article>
+        <img src="${artist.img_url}">
+        <h4>${artist.name}</h4>
+        <p>Genre: ${artist.genre}</p>
+        <p>Country: ${artist.country}</p>
+        </article>
+    `;
+    }
+    document.querySelector('#gridArtists').innerHTML = htmlTemplate;
+}
